e10350b3108c81adb1f217e56265cd3d7956f905
Kenneth Westerman <Kenny Westerman>
2022-03-03 13:58:46 -0500

    Add preliminary sleep and blood pressure variable prep.

--- scripts/prepare_phenotypes.R
+++ scripts/prepare_phenotypes.R
@@ -1,12 +1,30 @@
+## TO DO:
+- Must exclude individuals without genetics data prior to phenotyping (use existing of genetic PCs?)
+
+
+# Preliminaries ---------------------------------------------------------
+
 library(tidyverse)
 library(data.table)
 
+winsorize <- function(x, SDs=6) {
+  bounds <- mean(x, na.rm=TRUE) + c(-1, 1) * SDs * sd(x, na.rm=TRUE)
+  print(paste0(sum(x < bounds[1], na.rm=TRUE), " values winsorized at the lower bound."))
+  print(paste0(sum(x > bounds[2], na.rm=TRUE), " values winsorized at the upper bound."))
+  case_when(
+    x < bounds[1] ~ bounds[1],
+    x > bounds[2] ~ bounds[2],
+    TRUE ~ x
+  )
+}
 
-all_phenos_df <- fread("../data/raw/ukb47880.csv", nrows=1000, data.table=FALSE)
+basic_phenos_df <- fread("../data/raw/ukb47880.csv", nrows=10000, data.table=FALSE)
+blood_assays_df <- fread("../data/raw/ukb50493.csv", nrows=10000, data.table=FALSE)
+urine_assays_df <- fread("../data/raw/ukb50582.csv", nrows=10000, data.table=FALSE)
 
 # Covariates -------------------------------------------------------------
 
-basic_covar_df <- all_phenos_df %>%
+basic_covar_df <- basic_phenos_df %>%
   select(
     id = eid,
     age = `21022-0.0`,
@@ -14,10 +32,11 @@ basic_covar_df <- all_phenos_df %>%
     bmi = `21001-0.0`
   ) %>%
   mutate(
-    age_sq = age ** 2
+    age_sq = age ** 2,
+    sex = 1 - sex  # Switch coding to 0=Male, 1=Female
   )
 
-genetic_covar_df <- all_phenos_df %>%
+genetic_covar_df <- basic_phenos_df %>%
   select(
     id = eid,
     contains("22009-0")  # Genetic PC fields
@@ -28,22 +47,35 @@ covar_df <- left_join(basic_covar_df, genetic_covar_df, by="id")
 
 # Exposures --------------------------------------------------------------------
 
-smoking_df <- all_phenos_df %>%
+smoking_df <- basic_phenos_df %>%
   select(id = eid)
 
-alcohol_df <- all_phenos_df %>%
+alcohol_df <- basic_phenos_df %>%
   select(id = eid)
 
-sleep_df <- all_phenos_df %>%
+sleep_df <- basic_phenos_df %>%
+  select(
+    id = eid,
+    age = `21022-0.0`, 
+    sex = `31-0.0`,  # Coding shouldn't matter for regression adjustment
+    sleep_dur = `1160-0.0`
+  ) %>%
+  mutate(sleep_dur = ifelse(sleep_dur %in% c(-1, -3), NA, sleep_dur)) %>%
+  filter(!is.na(sleep_dur))
+sleep_df$sleep_dur_resid <- resid(lm(sleep_dur ~ sex * age, data=sleep_df))
+sleep_df <- sleep_df %>%
+  mutate(stst = as.integer(sleep_dur_resid <= quantile(sleep_dur_resid, 0.2)),
+	 ltst = as.integer(sleep_dur_resid >= quantile(sleep_dur_resid, 0.8))) %>%
+  select(id, sleep_dur_resid, stst, ltst)
+  
+
+depression_df <- basic_phenos_df %>%
   select(id = eid)
 
-depression_df <- all_phenos_df %>%
+pa_df <- basic_phenos_df %>%
   select(id = eid)
 
-pa_df <- all_phenos_df %>%
-  select(id = eid)
-
-education_df <- all_phenos_df %>%
+education_df <- basic_phenos_df %>%
   select(id = eid)
 
 exposure_df <- smoking_df %>%
@@ -55,24 +87,44 @@ exposure_df <- smoking_df %>%
 
 # Outcomes ---------------------------------------------------------------------
 
-ukb_biomarker_fields <- c(
-  alt = 30620, alb = 30600, alp = 30610, apoA = 30630, apoB = 30640,
-  ast = 30650, hscrp = 30710, Ca = 30680, chol = 30690, creatinine = 30700,
-  cysC = 30720, bilirubin_dir = 30660, ggt = 30730, glu = 30740, hba1c = 30750,
-  hdl = 30760, igf1 = 30770, ldl = 30780, lipA = 30790, oestradiol = 30800,
-  phos = 30810, rheum_factor = 30820, shbg = 30830, tes = 30850,
-  bilirubin_tot = 30840, protein_tot = 30860, tg = 30870, urate = 30880,
-  urea = 30670, vitD = 30890
-)
+#ukb_biomarker_fields <- c(
+#  chol = 30690, 
+#  glu = 30740, hba1c = 30750,
+#  hdl = 30760, ldl = 30780, 
+#  tg = 30870, 
+#)
 
-biomarker_df <- all_phenos_df %>%
+bp_df <- basic_phenos_df %>%
   select(
     id = eid,
-    # all_of(setNames(paste0(ukb_biomarker_fields, "-0.0"), 
-    #                 names(ukb_biomarker_fields)))
+    dbp1 = `4079-0.0`, dbp2 = `4079-0.1`,
+    sbp1 = `4080-0.0`, sbp2 = `4080-0.1`,
+    contains("6177-0.")  # Self-reported cholesterol/BP/insulin medication use
+  ) %>%
+  mutate(
+    dbp = (dbp1 + dbp2) / 2,
+    sbp = (sbp1 + sbp2) / 2,
+    bp_med = rowSums(.[, paste0("6177-0.", 0:2)] == 2, na.rm=TRUE) >= 1,
+    dbp = ifelse(bp_med, dbp + 10, dbp),
+    sbp = ifelse(bp_med, sbp + 15, sbp),
+    pp = sbp - dbp
+  ) %>%
+  mutate_at(vars(dbp, sbp, pp), winsorize) %>%
+  select(id, dbp, sbp, pp, bp_med)
+
+
+biomarker_df <- blood_assays_df %>%
+  select(
+    id = eid
+    #all_of(setNames(paste0(ukb_biomarker_fields, "-0.0"), 
+    #                names(ukb_biomarker_fields)))
   )
 
-outcome_df <- biomarker_df
+outcome_df <- full_join(
+  bp_df, 
+  biomarker_df,
+  by="id"
+)
 
 # Final processing -------------------------------------------------------------
 
